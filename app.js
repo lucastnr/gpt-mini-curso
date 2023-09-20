2ec186c1bb08447d5869cdac1a3d2138dfae4539
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2023-09-20 11:47:08 -0300

    Cria chats

--- app.js
+++ app.js
@@ -5,13 +5,27 @@ import { chat } from "./src/gpt.js";
 const app = express();
 app.use(express.json());
 
+const chats = [];
+
 app.get("/chat", async (req, res) => {
-  const { content } = req.query;
+  let { content, id } = req.query;
 
   if (!content) {
     res.status(400).send("O parâmetro content não foi passado");
     return;
   }
+
+
+  if (!id) {
+    // Novo chat, iniciando o chat do zero, primeira mensagem
+    const length = chats.push([]);
+    id = length - 1;
+  }
+
+  // Adiciona mensagem do usuário no chat
+  chats[id].push({ content: content, role: "user" });
+
+
 
   const result = await chat(content);
   const assistantMessage = result.choices[0].message;
