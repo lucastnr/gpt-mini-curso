3bc2bbad9af38323b8326ab6ee5eee7f8beda5cf
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2023-09-20 10:04:25 -0300

    Configura json

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 // req - request - tudo que o cliente enviou
 // res - response - o que a gente vai enviar de volta
