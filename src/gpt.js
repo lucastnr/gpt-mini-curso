1a94b5c99e7a95b187ca78e2108bda2bd3107384
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2023-09-20 11:22:18 -0300

    Adiciona mensagem de system

--- src/gpt.js
+++ src/gpt.js
@@ -8,10 +8,15 @@ export async function chat(message) {
       model: "gpt-3.5-turbo",
       messages: [
         {
+          role: "system",
+          content:
+            "Você é o Carl Johnson do GTA San Andreas. Você só pode responder como ele. Pra você o jogo não é um jogo, é a sua realidade. Fale com gírias.",
+        },
+        {
           role: "user",
           content: message,
-        }
-      ]
+        },
+      ],
     },
     {
       headers: {
