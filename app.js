4f0a39a6f635407c3a2d872a6aa6fe2936740aa1
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2023-09-20 10:10:52 -0300

    Adiciona static

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-// req - request - tudo que o cliente enviou
-// res - response - o que a gente vai enviar de volta
-app.get("/", (req, res) => {
-  res.send("Oi");
-});
+app.use("/", express.static("public"))
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
