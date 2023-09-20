46f093a88f5cd5663086acf27ee5461296720747
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2023-09-20 10:41:53 -0300

    Adiciona req do axios

--- src/gpt.js
+++ src/gpt.js
@@ -3,7 +3,15 @@ import axios from "axios";
 async function chat(message) {
   axios.post(
     "https://api.openai.com/v1/chat/completions",
-    {},
+    {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: message,
+        }
+      ]
+    },
     {
       headers: {
         Authorization: "Bearer " + process.env.OPENAI_API_KEY,
