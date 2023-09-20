f4566ea2837a307584e0a1c3440b4cabe876465b
Lucas Tenório <59018759+lucastnr@users.noreply.github.com>
2023-09-20 10:03:15 -0300

    Primeira rota

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+// req - request - tudo que o cliente enviou
+// res - response - o que a gente vai enviar de volta
+app.get("/", (req, res) => {
+  res.send("Oi");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
